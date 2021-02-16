112e998900e4075f316f19d59995fa87c0c7d77c
OtsuKotsu <otoxukk.y@gmail.com>
2021-02-17 00:21:44 +0900

    now impl

---
+++ lib/graph/kruskal.hpp
@@ -0,0 +1,11 @@
+// Kruskal's algorithm
+// Verified : 
+//
+#include "lib/graph/graph_template.hpp"
+#include "lib/data_structure/disjoint_set_union.hpp"
+#include <bits/stdc++.h>
+
+
+/* Kruskal's algorithm */
+// construct Minimum Spanning Tree
+// time complexity : O()
