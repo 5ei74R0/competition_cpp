40ccf65a0132c32f9911067ded0d383fa5cef38f
OtsuKotsu <otoxukk.y@gmail.com>
2021-04-03 21:35:20 +0900

    Modify template

--- src/run3.cpp
+++ src/run3.cpp
@@ -1,84 +1,93 @@
 #include <bits/stdc++.h>
-using namespace std;
 
-#define debug(x) cout <<"DEBUG "<< #x << ": " << (x) << '\n'
-#define rep(i,n) for(int_fast32_t i = 0; i < ((int_fast32_t)(n)); ++i)   // 0-indexed up
-#define rep1(i,n) for(int_fast32_t i = 1; i <= ((int_fast32_t)(n)); ++i) // 1-indexed up
-#define rrep(i,n) for(int_fast32_t i = ((int_fast32_t)(n)-1); i >= 0; --i)  // 0-indexed down
-#define rrep1(i,n) for(int_fast32_t i = ((int_fast32_t)(n)); i >= 1; --i)   // 1-indexed down
+#define debug(x) std::cout <<"DEBUG "<< #x << ": " << (x) << '\n'
+#define rep(i,n) for (std::size_t i = 0; i < ((std::size_t)(n)); ++i)
 #define all(x) (x).begin(),(x).end()
 
-
-template<typename T>
-using vec = vector<T>;
-using i32 = int_fast32_t;
-using i64 = int_fast64_t;
-using u32 = uint_fast32_t;
-using u64 = uint_fast64_t;
+// type alias
+using i32 = std::int_fast32_t;
+using i64 = std::int_fast64_t;
+using u32 = std::uint_fast32_t;
+using u64 = std::uint_fast64_t;
 using ld = long double;
-using vi = vec<int_fast32_t>;
-using vl = vec<int_fast64_t>;
+
+template <typename T> using vec = std::vector<T>;
+template <class T> using maxheap = std::priority_queue<T>;
+template <class T> using minheap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+using vi = vec<i32>;
+using vl = vec<i64>;
 using vld = vec<ld>;
 using vvi = vec<vi>;
-using PII = pair<int_fast32_t, int_fast32_t>;
+using PII = std::pair<i32, i32>;
 
-template<class T>
-using maxheap = priority_queue<T>;
-template<class T>
-using minheap = priority_queue<T, vector<T>, greater<T>>;
-
-template<class T,class U>
-inline bool chmax(T &a, const U &b) {
-    if (a < b) {
-        a = b;
-        return true;
-    }
-    return false;
-}
-template<class T,class U>
-inline bool chmin(T &a, const U &b) {
-    if (a > b) {
-        a = b;
-        return true;
-    }
-    return false;
-}
-
-// ostream expension
-template <class T, class U>
-std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& p) {
+// ostream-expension
+template <class T, class U> std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& p) {
     os << "(" << p.first << ", " << p.second << ")";
     return os;
 }
 
-template <class T>
-std::ostream& operator<<(std::ostream& os, const std::vector<T>& v) {
+template <class T> std::ostream& operator<<(std::ostream& os, const std::vector<T>& v) {
+    std::size_t i = 0;
     os << "[";
-    for(size_t i = 0; i < v.size(); ++i) {
-        os << (i ? ", " : "") << v[i];
-    }
-    os << "]";
+    for (const auto& val : v) os << val << (++i < v.size() ? ", " : "]");
     return os;
 }
 
-const ld Pi = std::acos(-1.0L);
-constexpr i64 infll = (1LL<<62)-1;
-constexpr i32 inf = (1<<30)-1;
+template <class T> std::ostream& operator<<(std::ostream& os, const std::set<T>& st) {
+    std::size_t i = 0;
+    os << "{";
+    for (const auto& val : st) os << val << (++i < st.size() ? ", " : "}");
+    return os;
+}
 
+template <class T, class U> std::ostream& operator<<(std::ostream& os, const std::map<T, U>& mp) {
+    std::size_t i = 0;
+    os << "{";
+    for (const auto& p : mp) os << p.first << ": " << p.second << (++i < mp.size() ? ", " : "}");
+    return os;
+}
+
+// constants
+constexpr ld pi = M_PIf64x;
+constexpr i64 infll = std::numeric_limits<i64>::max() >> 1;
+constexpr i32 inf = std::numeric_limits<i32>::max() >> 1;
 
 /* Class & Function */
+template <class T, class U> inline bool chmax(T &a, const U &b) {
+    if (a < b) { a = b; return true; }
+    return false;
+}
 
+template <class T, class U> inline bool chmin(T &a, const U &b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+
+template <class T>
+auto make_vec(std::size_t n, const T &value) {
+    return std::vector<T>(n, value);
+}
+template <class... Args>
+auto make_vec(std::size_t n, Args... args) {
+    return std::vector<decltype(make_vec(args...))>(n, make_vec(args...));
+}
+
+namespace space_575 {  // unsafe: Name collisions can occur.
+    using namespace std;
+
+    // edit functions here
+
+};
 
 /* Main */
 signed main() {
-    double StartTimeOfMainFunction = clock();
+    using namespace space_575;
 
     // fast io
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
     // cout << fixed << setprecision(15);  // fix io precision
 
+    
 
-
-    // printf("Elapsed time: %5.3f ms\n",1000.*((clock()-StartTimeOfMainFunction)/CLOCKS_PER_SEC));
 }
