65b8a7438e8d750cc2c552f87aef49e444781bf4
OtsuKotsu <otoxukk.y@gmail.com>
2021-04-06 12:12:33 +0900

    Add file of sqrt_tree (has not written)

--- lib/data_structure/README.md
+++ lib/data_structure/README.md
@@ -3,6 +3,8 @@ Data Structures
 
 ## directories
 - [disjoint_set_union.hpp](./disjoint_set_union.hpp)
+- [monoid.hpp](./monoid.hpp)
 - [segment_tree.hpp](./segment_tree.hpp)
 - [lazy_segment_tree.hpp](./lazy_segment_tree.hpp)
+- [sqrt_tree.hpp](./sqrt_tree.hpp)
 - [trie_simple.hpp](./trie_simple.hpp)
---
+++ lib/data_structure/sqrt_tree.hpp
@@ -0,0 +1,23 @@
+// SqrtTree
+// replying to queries for a segment in O(1) time, and modifying one element in O(\sqrt N)
+// Verified : <url>
+
+// dependencies
+// <#include "lib/hoge.hpp">
+// <#include <bits/stdc++.h>>
+
+
+/* SqrtTree */
+// replying to queries for a segment in O(1) time, and modifying one element in O(\sqrt N)
+//
+// List of Member
+// param:
+// 1. <p>: <type>
+// function:
+// 1. <f>(<p>: <type>) -> <type>
+template <class Monoid> class SqrtTree {
+  public:
+
+  private:
+    
+};
