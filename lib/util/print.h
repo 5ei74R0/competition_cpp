e0b2cf86e9856b95e969b935be43850e6e9e3cdc
OtsuKotsu <otoxukk.y@gmail.com>
2022-02-10 15:14:42 +0900

    add print, traits

---
+++ lib/util/print.h
@@ -0,0 +1,43 @@
+#if !defined(UTIL_PRINT_H)
+#define UTIL_PRINT_H
+
+#include <iostream>
+#include <utility>
+
+#include "../base/traits.h"
+
+namespace internal {
+template <class... Args>
+constexpr auto false_t = false;
+
+template <char separator, char end, std::size_t N = 0, class T>
+void print_tuplelike_impl(const T& obj, std::ostream& os = std::cout) {
+  if constexpr (N < std::tuple_size<T>::value) {
+    os << std::get<N>(obj)
+       << (N + 1 < std::tuple_size<T>::value ? separator : end);
+    print_tuplelike_impl<separator, end, N + 1>(obj, os);
+  }
+}
+template <char separator, char end, class Iterable>
+void print_iterable_impl(Iterable iterable, std::ostream& os = std::cout) {
+  auto iter = std::begin(iterable), last = std::end(iterable);
+  while (iter != last) os << *iter << (++iter != last ? separator : end);
+}
+}  // namespace internal
+
+template <char separator = ' ', char end = '\n', class T>
+void print(const T& obj, std::ostream& os = std::cout) {
+  if constexpr (std::is_same<T, bool>::value) {
+    os << (obj ? "Yes" : "No") << end;
+  } else if constexpr (traits::is_printable<T>::value) {
+    os << obj << end;
+  } else if constexpr (traits::is_iterable<T>::value) {
+    internal::print_iterable_impl<separator, end>(obj, os);
+  } else if constexpr (traits::is_tuplelike<T>::value) {
+    internal::print_tuplelike_impl<separator, end>(obj, os);
+  } else if constexpr (internal::false_t<T>) {  // compile time assert
+    static_assert(internal::false_t<T>, "incompatible type error in print()");
+  }
+}
+
+#endif  // UTIL_PRINT_H
